8fd043ccd4a7596ba4aca237b8eb7c8c1a3e6204
Ilade-s <76600778+Ilade-s@users.noreply.github.com>
2021-03-29 08:33:36 +0200

    suite

--- ForestFire.cpp
+++ ForestFire.cpp
@@ -8,6 +8,18 @@
 
 using namespace std;
 
+void print(vector<vector<int>> Grid, int cols, int rows)
+{
+    // Affichage grille
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << Grid[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
 vector<vector<int>> GenGrid(int rows, int cols, float TreeRate) // Génération de la grille
 {
     // Initialisation variables
@@ -15,7 +27,7 @@ vector<vector<int>> GenGrid(int rows, int cols, float TreeRate) // Génération 
     vector<vector<int>> Grid;
 
     // Création grille en ligne
-    for (int i = 0; i < rows*cols*(1-TreeRate)-1; ++i) GridLine.push_back(0); // Cases vides
+    for (int i = 0; i < rows*cols*(1-TreeRate); ++i) GridLine.push_back(0); // Cases vides
     for (int i = 0; i < rows*cols*TreeRate; ++i) GridLine.push_back(1); // Cases de forêt
     GridLine[0] = 2; // Case en feu initiale
 
@@ -37,6 +49,31 @@ vector<vector<int>> GenGrid(int rows, int cols, float TreeRate) // Génération 
     return Grid;
 }
 
+void PropFire(vector<vector<int>> Grid) // exécute une passe de propagation de feu
+{
+
+
+
+}
+
+int TreesRemaining(vector<vector<int>> Grid, int cols, int rows) // fonction pour compter les arbres restants
+{
+    int Ntrees = 0;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (Grid[i][j] == 1)
+            {
+                Ntrees++;
+            }
+            
+        }
+    }
+    return Ntrees;
+}
+
 int main()
 {
     cout << "Bienvenue dans ma simulation de feu de foret !\n";
@@ -45,19 +82,12 @@ int main()
     int rows = 50;
     int cols = 50;
     float TreeRate = 0.90;
-    vector<vector<int>> Grid = GenGrid(rows, cols, TreeRate);
+    vector<vector<int>> Grid = GenGrid(rows, cols, TreeRate); // création grille
+    int Ntrees = TreesRemaining(Grid, cols, rows); // nombre d'arbres
 
-    // Affichage grille
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            cout << Grid[i][j] << " ";
-        }
-        cout << "\n";
-    }
+    print(Grid, cols, rows); // affichage grille
 
-    cout << "\n";
+    cout << "\n" << Ntrees << "\n";
 
     return 0;
 }
